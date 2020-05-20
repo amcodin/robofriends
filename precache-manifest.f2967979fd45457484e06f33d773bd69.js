37e2141f86a54f8d8257894a08445d8d368dcf9d
amcodin <amichel.tech@gmail.com>
2020-05-20 15:33:56 +1000

    Updates

---
+++ precache-manifest.f2967979fd45457484e06f33d773bd69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661f0f15ce5cb027be39073bfc515e10",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0151ece8fa64682a6600",
+    "url": "/robofriends/static/css/main.dae7424a.chunk.css"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/robofriends/static/js/2.12e19136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.12e19136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0151ece8fa64682a6600",
+    "url": "/robofriends/static/js/main.913318e4.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
